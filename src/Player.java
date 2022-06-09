6c940c0b9854cef8c1d047178499c6b82b3bea17
arriosme <arios@novopangea.com.co>
2022-06-08 23:47:53 -0500

    Nombre del player

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ganando con Java");
+		String nombreplayer ="Jugador";
+		
+	}
+
+}
