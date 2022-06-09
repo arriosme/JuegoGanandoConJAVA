2a20dd8f1a9c328c45f0e587bdb2c2d0207b3b45
arriosme <arios@novopangea.com.co>
2022-06-09 00:42:07 -0500

    Cambios en Score

--- src/Score.java
+++ src/Score.java
@@ -3,7 +3,16 @@ public class Score {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String scoreplayer ="Su puntaje logrado fue: " +" puntos";
+		System.out.println(scoreplayer);
+		xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+		xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+		xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+		// 
+		/*
+		 * Validar
+		 */
 	}
 
 }
+
